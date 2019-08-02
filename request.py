da19261e7a47b258a7d7979a3c9ea190c00f036b
sAnjali12 <anjalisen060@gmail.com>
2019-08-02 22:19:21 +0530

    Print the childexercises slug and using next and previous data by if condition

--- request.py
+++ request.py
@@ -6,7 +6,7 @@ import pathlib
 
 BASE_URL = "http://saral.navgurukul.org/api/courses"
 coursesJsonData={}
-exerciseJsonData = {}
+exercisData = {}
 def printText(text):
     print ("\n\n\t"+"*"*20+text+"*"*20+"\t\n\n")
 
@@ -56,10 +56,10 @@ getCourses(coursesJsonData)
 
 printText("*"*10)
 
-# I created a function to take userInput from the user.
-userInput = input("Enter your course number :- ")
-courseId = (coursesJsonData["availableCourses"][int(userInput)]["id"])
-courseName=(coursesJsonData["availableCourses"][int(userInput)]["name"])
+# I created a function to take users from the user.
+users = input("Enter your course number :- ")
+courseId = (coursesJsonData["availableCourses"][int(users)]["id"])
+courseName=(coursesJsonData["availableCourses"][int(users)]["name"])
 print (courseId)
 
 # Calling  API for print the exercises of the courses.
@@ -67,29 +67,108 @@ print (courseId)
 printText("EXERCISES OF COURSES")  
 print (courseName)
 exerciseUrl = BASE_URL+"/"+str(courseId)+"/exercises"
-exerciseJsonData = getData("exercises_"+str(courseId)+".json",exerciseUrl)
-sluglist = []
+exercisData = getData("exercises_"+str(courseId)+".json",exerciseUrl)
 exercisesIdList = []
-
+sluglist = []
 def getExercises(exercisesData):
     
-    index = 1
-    while index<len(exercisesData["data"]):
-        print  (str(index)+"."+exercisesData["data"][index]["name"])
-        exercisesIdList.append(exercisesData["data"][index]["id"])
-    # print exercisesIdList
-        index1 = 0
-        while index1<len(exercisesData["data"][index]["childExercises"]):
-            print ("\t"+str(index1+1)+":"+exercisesData["data"][index]["childExercises"][index1]["name"])
-            sluglist.append(exercisesData["data"][index]["parentExerciseId"][index]["childExercises"][index1]["slug"])
-            
-            index1 += 1
-        index = index+1
-    return sluglist
-    # print exercisesIdList
-    
-
-# print (exerciseJsonData)
+    index1 = 1
+    while index1<len(exercisesData["data"]):
+        useExercisesId = exercisesData["data"][index1]["id"]
+        print  str(index1)+"."+exercisesData["data"][index1]["name"],useExercisesId
+        slug=exercisesData["data"][index1]["slug"]
+        sluglist.append(slug)
         
-slugListName = getExercises(exerciseJsonData)
-print slugListName
+        exercisesIdList.append(exercisesData["data"][index1]["id"])
+
+        index2 = 0
+        while index2<len(exercisesData["data"][index1]["childExercises"]):
+            print ("\t"+str(index2+1)+":"+exercisesData["data"][index1]["childExercises"][index2]["name"])
+            index2 += 1
+        index1 = index1+1  
+    return sluglist
+        
+exercisesId = getExercises(exercisData)
+
+userWant = raw_input("IF YOU WANT TO GO UP SIDE SO ENTER (up)")
+if userWant == "up":
+    print getCourses(coursesJsonData)
+else:
+
+    #USING 3TH API FOR PRINT PERENTEXERCISE SLUG
+
+    users = input("Enter Your  Choice Exercises ID")
+    inputId  = exercisesId[users-1]
+
+    printText("CONTENT OF EXERCISES")
+    useExercisesId = exercisData["data"][users]["id"]
+    contentUrl = BASE_URL+"/"+str(users)+"/"+"exercise"+"/"+"getBySlug?slug="+str(inputId)
+    contentJsonData = getData("content_"+str(users)+".json",contentUrl)
+    content = contentJsonData["content"]
+    print "\n YOUR CHOICE ID CONTENT\n"
+    print content
+    printText("*"*10)
+
+    # FOR CHILDEXERCISES SLUG USING 3TH API.
+    chidExerciseData = exercisData["data"][users]["childExercises"]
+    childSlug=[]
+    index3 = 0
+    while index3<len(chidExerciseData):
+        print ("\t"+str(index3+1)+":"+chidExerciseData[index3]["name"])
+        childSlug.append(chidExerciseData[index3]["slug"])
+        index3+=1
+
+    user1 = input("CHOICE YOUR LASSON")
+    input1Id = childSlug[user1-1]
+
+    printText("CONTENT OF CHILDEXERCIS :)")
+    child_ID = exercisData["data"][users]["childExercises"][user1-1]["id"]
+    print child_ID
+    cContentURl = BASE_URL+"/"+str(child_ID)+"/"+"exercise"+"/"+"getBySlug?slug="+str(input1Id)
+    def childData(child_ID,cContentURl):
+        print cContentURl
+        print child_ID
+        childJson = getData("childContent_"+str(child_ID)+".json",cContentURl)
+        childContent = childJson["content"]
+        return childContent
+
+
+    slugData = childData(child_ID,cContentURl)
+    print slugData
+
+    #USING WHILE LOOP FOR NEXT AND PREVIOUS CONTENT. 
+
+    index4  = 0
+    while index4<len(childSlug):
+        userNextP = raw_input("IF YOU WANT NEXT CONTENT SO ENTER (n) AND IF YOU WANT PREVIOUS CONTENT SO ENTER (P) :)")
+        if userNextP == "n":
+            user1 = user1+1
+            input2Id = (childSlug[user1-1])
+            print input2Id
+            nextSlugId = exercisData["data"][users]["childExercises"][user1-1]["id"]
+            nextcontentURL = BASE_URL+"/"+str(nextSlugId)+"/"+"exercise"+"/"+"getBySlug?slug="+str(input2Id)
+            # print nextcontentURL
+            nextcontent = childData(nextSlugId,nextcontentURL)
+            print nextcontent
+            # print user1
+            # print nextSlugId
+            # user1=user1+1
+        elif userNextP == "p":
+            users3 = user1-1
+            if users3 == 0:
+                break
+    
+            else:
+                input3Id = (childSlug[users3])
+                previouSlugId = exercisData["data"][users]["childExercises"][users3-1]["id"]
+                preContentURL = BASE_URL+"/"+str(previouSlugId)+"/"+"exercise"+"/"+"getBySlug?slug="+str(input3Id)
+                previouscontent = childData(previouSlugId,preContentURL)
+                print previouscontent
+                print users3
+        index4 += 1
+    else:
+        print "INDEX OUTOF REANGE"
+
+
+
+    
